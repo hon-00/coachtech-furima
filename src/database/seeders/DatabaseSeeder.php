ce4cd92c2203fc1e2ab9eecf7eef0229d773e024
Hono <suinaixiangaocun@gmail.com>
2025-09-19 01:39:58 +0900

    FactoryとSeederを追加・修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CategoriesTableSeeder::class,
+        ]);
+
+        if (app()->environment(['local', 'testing'])) {
+            \App\Models\User::factory(5)->create()->each(function ($user) {
+                $items = \App\Models\Item::factory(2)->create(['user_id' => $user->id]);
+                foreach ($items as $item) {
+                    \App\Models\Comment::factory(2)->create([
+                        'user_id' => $user->id,
+                        'item_id' => $item->id,
+                    ]);
+                }
+            });
+        }
     }
 }
