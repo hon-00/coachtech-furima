0788029dbdd77d049e662b9931e3d4b4628332b2
Hono <suinaixiangaocun@gmail.com>
2025-09-19 16:23:40 +0900

    feat: コメントと商品用のFactoryを追加

---
+++ src/database/factories/ItemFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+class ItemFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph(),
+            'condition' => $this->faker->randomElement(['良好', '目立った傷や汚れなし', 'やや傷や汚れあり', '状態が悪い']),
+            'price' => $this->faker->numberBetween(500, 50000),
+            'brand' => $this->faker->optional()->company(),
+            'image' => 'dummy.jpg',
+            'sold_flag' => false,
+        ];
+    }
+}
