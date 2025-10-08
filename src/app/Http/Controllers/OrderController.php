b367bd24dfc8c86ec5583de06f32c8e9d0889207
kaho <suinaixiangaocun@gmail.com>
2025-10-08 15:58:55 +0900

    Revert "feat: 購入画面からの住所変更ページ実装とバリデーション追加"

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
-use App\Http\Requests\AddressRequest;
 
 class OrderController extends Controller
 {
@@ -27,31 +26,5 @@ class OrderController extends Controller
         // Order::create([...]);
 
         return redirect()->route('home')->with('success', '購入が完了しました');
-    }
-
-    public function editAddress(Request $request, $item_id)
-    {
-        $item = Item::findOrFail($item_id);
-        $user = auth()->user();
-
-        return view('orders.edit_address', compact('item', 'user'));
-    }
-
-    public function updateAddress(AddressRequest $request, $item)
-    {
-        $item = Item::findOrFail($item);
-        $user = auth()->user();
-
-        // バリデーション済みデータを取得
-        $validated = $request->validated();
-
-        // ユーザーの住所を更新
-        $user->update([
-            'postal_code' => $validated['postal_code'],
-            'address'     => $validated['address'],
-            'building'    => $validated['building'] ?? null,
-        ]);
-
-        return redirect()->route('purchase.create', ['item' => $item->id])->with('success', '住所が更新されました');
     }
 }
