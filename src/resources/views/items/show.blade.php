067ce9b0bff0a2d1caf5a434718445ed6792984e
Hono <suinaixiangaocun@gmail.com>
2025-10-15 00:48:33 +0900

    refactor: CSSフォルダ名変更に伴う参照パスの修正

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/item/show.css') }}">
+<link rel="stylesheet" href="{{ asset('css/items/show.css') }}">
 @endsection
 
 @section('content')
