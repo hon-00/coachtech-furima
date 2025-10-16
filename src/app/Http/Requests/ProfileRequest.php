12a04866f78fbc1cec81ef1b09490255eb2711db
Hono <suinaixiangaocun@gmail.com>
2025-10-17 00:07:54 +0900

    feat: プロフィール編集・購入処理のバリデーション追加

---
+++ src/app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        return [
+            'profile_image' => 'nullable|image|mimes:jpeg,png',
+            'name' => 'required|string|max:20',
+            'postal_code' => ['required', 'regex:/^\d{3}-\d{4}$/'],
+            'address' => 'required|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'profile_image.image' => 'プロフィール画像は画像ファイルにしてください。',
+            'profile_image.mimes' => 'プロフィール画像はJPEGまたはPNG形式でアップロードしてください。',
+            'name.required' => 'ユーザー名を入力してください。',
+            'name.max' => 'ユーザー名は20文字以内で入力してください。',
+            'postal_code.required' => '郵便番号を入力してください。',
+            'postal_code.regex' => '郵便番号はハイフンありで8文字にしてください。',
+            'address.required' => '住所を入力してください。',
+        ];
+    }
+}
